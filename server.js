797b3300531243ffd2cf6aedc04e64553ac4e8bb
Adam <adam@Fuchsias-iMac.local>
2015-10-25 21:48:32 +1100

    cors setup

--- server.js
+++ server.js
@@ -2,13 +2,32 @@ var express = require('express');
 var bodyParser = require('body-parser');
 	listings = require('./routes/listings');
 
+//setup for CORS access
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 
 
 var app = express();
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
+
+app.configure(function() { 
+	app.use(allowCrossDomain);
+	app.use( bodyParser.json() );       // to support JSON-encoded bodies
+	app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	  extended: true
+	}));
+});
 
 app.get('/', listings.startScreen);
 app.get('/listings', listings.findAll);
