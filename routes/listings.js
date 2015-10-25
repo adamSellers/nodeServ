ee902113d02ac7842e95535877daa978d8703c04
sellbot <adza007@gmail.com>
2015-10-26 06:46:07 +1100

    on the train

--- routes/listings.js
+++ routes/listings.js
@@ -38,8 +38,8 @@ exports.updateListing = function(req, res) {
     var listingId = req.params.listingId;
     var results = [];
 
-    var jssaleprice = req.body.saleprice;
-    var jssaledate = req.body.saledate;
+    var jssaleprice = req.body.sale_price__c;
+    var jssaledate = req.body.sale_date__c;
 
     console.log('saleprice is: ' + jssaleprice + ' and saledate is: ' + jssaledate);
     console.log('listing id is: ' + listingId);
@@ -55,9 +55,9 @@ exports.updateListing = function(req, res) {
 	  	}
 
 	  	//SQL query to update data
-	  	client.query('UPDATE salesforce.Listing__c SET saleprice = ($1), saledate = ($2) WHERE id = ($3)', [jssaleprice, jssaledate, listingId]);
+	  	client.query('UPDATE salesforce.Listing__c SET sale_price__c = ($1), sale_date__c = ($2) WHERE id = ($3)', [jssaleprice, jssaledate, listingId]);
 	  	//query back the data set
-	 	var query = client.query('SELECT id, information, bath, parking, saleprice, saledate FROM salesforce.Listing__c WHERE id = ($1)', [listingId]);
+	 	var query = client.query('SELECT id, Bath__c, Beds__c, CreatedDate, Description__c, Information__c, Mobile_Image__c, Name, Parking__c, Property_Id__c, RPP_Market_Value__c, Sale_Date__c, Sale_Price__c, Street__c, Suburb__c, Type__c FROM salesforce.Listing__c WHERE id = ($1)', [listingId]);
 
 	  	//stream results back one at a time
 	  	query.on('row', function(row) {
